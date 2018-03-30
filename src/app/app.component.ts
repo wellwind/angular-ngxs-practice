b728dd778fd44b7a91f24125940ffffa5cce0d91
黃升煌 Mike <mike@miniasp.com>
2018-03-30 13:06:17 +0800

    Load todo items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs/Observable';
+import { TodoItem, GetTodoItems, TodoState } from './states/todo/todo-state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @Select(TodoState) todos$: Observable<TodoItem[]>;
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.store.dispatch(new GetTodoItems());
+  }
 }
--- src/app/states/todo/todo-state.ts
+++ src/app/states/todo/todo-state.ts
@@ -1,10 +1,18 @@
-import { State } from '@ngxs/store';
+import { State, Action, StateContext } from '@ngxs/store';
 
 export class TodoItem {
-  constructor(public payload: string) {}
+  constructor(public name: string, public isCompleted: boolean) {}
 }
 
+export class GetTodoItems {}
+
 @State<TodoItem[]>({
-  name: 'todos'
+  name: 'todos',
+  defaults: [new TodoItem('todo 1', true), new TodoItem('todo 2', false), new TodoItem('todo 3', false)]
 })
-export class TodoState {}
+export class TodoState {
+  @Action(GetTodoItems)
+  getTodoItems(stateContext: StateContext<TodoItem[]>) {
+    stateContext.setState([new TodoItem('new-todo 1', true), new TodoItem('new-todo 2', false), new TodoItem('new-todo 3', false)]);
+  }
+}
